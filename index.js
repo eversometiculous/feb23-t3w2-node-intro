9ec7444a424be836a0a0cff700fc7fd85d0f9071
eversometiculous <126760003+eversometiculous@users.noreply.github.com>
2023-08-24 20:57:36 +1000

    updated some code

--- index.js
+++ index.js
@@ -1,35 +1,71 @@
-const promptSync = require('prompt-sync');
+const promptSync = require('prompt-sync');          // all require stuff should be at the top of the code, to make things easier!!
 
-const promptInstance = promptSync({
-    sigint: true        // allows CTRL+C to exit the app
-});
+class SomeCustomError extends Error{
+    constructor(message = "Some error has happened, write a better message lol!") {
+        super();
 
-// some comment here
-// comment for naomi
+        this.statusCode = 404;
 
-// or const promptInstance = require('prompt-sync')({sigint:true});
-
-let userInput = null;
-let userEnteredNumber = false;
-
-do {
-    userInput = promptInstance("What is the coolest number in the universe? ");
-
-    if (!parseInt(userInput) && !parseFloat(userInput)) {
-        //throw "User did not enter a number!";
-        throw new Error("User did not enter a number!!");       //another style
+        this.message = message;
     }
+}
 
-    let userInputAsNumber = Number(userInput);
-
-    if (userInputAsNumber === 42){
-        console.log("The universe thanks you for figuring out the answer!");
-        userEnteredNumber = true;
+function app(){
+    const promptInstance = promptSync({
+        sigint: true        // allows CTRL+C to exit the app
+    });
     
+    // some comment here
+    // comment for naomi
+    
+    // or const promptInstance = require('prompt-sync')({sigint:true});
+    
+    let userInput = null;
+    let userEnteredNumber = false;
+    
+    do {
+        userInput = promptInstance("What is the coolest number in the universe? ");
+    
+        if (!parseInt(userInput) && !parseFloat(userInput)) {
+            //throw "User did not enter a number!";
+            throw new Error("User did not enter a number!!");       //another style
+        }
+    
+        let userInputAsNumber = Number(userInput);
+    
+        if (userInputAsNumber === 42){
+            console.log("The universe thanks you for figuring out the answer!");
+            userEnteredNumber = true;
+        
+        } else {
+            console.log("The universe seems unfulfilled...");
+        }
+    } while (!userEnteredNumber);
+}
+
+try {
+    app();
+    console.log("App executed successfully!");
+} catch (error) {
+    if (error.statusCode){
+        console.log(`Error ${error.statusCode} occurred, its message is ${error.message}`);
     } else {
-        console.log("The universe seems unfulfilled...");
+        console.log("An error has occurred!" + error.message);
     }
-} while (!userEnteredNumber);
+} finally   {
+    console.log("App has finished running. Pretend there's nice DB closing code here!")
+}
+
+/*
+try {
+    some code to execute
+}   catch   (some Error instance)   {
+    some error-handling code
+}   finally {
+    close up smoothly and safely without instantly crashing or otherwise quickly closing
+}
+
+app()
 
 // helloWorld(userInput);
 
